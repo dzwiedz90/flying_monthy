63e61c193b4b7416f69ff0e7d20d52d420e556b9
Mateusz-Kluska <mat.kluska@gmail.com>
2019-10-26 15:34:04 +0200

    Adding User Model

--- templates/models.py
+++ templates/models.py
@@ -1,14 +1,3 @@
-from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=32, blank=False)
-    password = models.CharField(max_length=32, blank=False)
-    email = models.CharField(max_length=64, blank=False)
-    account_creation_date = models.DateTimeField(auto_now_add=True,
-                                                 auto_now=False, blank=False)
-    first_name = models.CharField(max_length=64, default='')
-    last_name = models.CharField(max_length=64, default='')
-    is_staff = models.BooleanField(default=False, blank=False)
-    number_of_memes = models.IntegerField(default=0, blank=False)
