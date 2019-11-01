920c50b92aea7c6e2b2d20452c6764349e13c3a7
Mateusz-Kluska <mat.kluska@gmail.com>
2019-11-01 20:10:42 +0100

    Category class

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
