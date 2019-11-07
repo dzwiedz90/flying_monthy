77b14c2cdf2c808868c262e887313e2c9c2f1add
Adam Rogacewicz <adam.rogacewicz@gmail.com>
2019-11-07 19:35:34 +0100

    adding REST for register and login

--- users/models.py
+++ users/models.py
@@ -1,11 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+# imports for token (REST API)
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
-# Create your models here.
-
+# we have to put this code to other app !!!
 class Category(models.Model):
     name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+
+
+
+
